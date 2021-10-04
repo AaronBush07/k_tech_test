771216c6574f28d71e6d1447b5d41c1a6cc93e6c
Aaronbush07 <Aaron.bush07@gmail.com>
2021-10-04 22:47:27 +1100

    colours class

--- backend/backend/colours.py
+++ backend/backend/colours.py
@@ -1,2 +1,48 @@
-class RGB(): 
-    pass
+import random
+from abc import ABC, abstractmethod
+
+
+class Colour(ABC):
+    @abstractmethod
+    def props(self):
+        pass
+
+
+class RGB(Colour): 
+    def __init__(self):
+        self.type="RGB"
+        self.red = random.randrange(0,255,1) 
+        self.blue = random.randrange(0,255,1) 
+        self.green = random.randrange(0,255,1) 
+    def props(self):
+        data = {
+            "type": self.type,
+            "red": self.red,
+            "blue": self.blue,
+            "green": self.green
+        } 
+        return data
+
+class HSL(Colour):  
+    def __init__(self):
+        self.type="HSL"
+        self.hue = random.randrange(0,360,1) 
+        self.saturation = random.randrange(0,1000,1) 
+        self.lightness = random.randrange(0,1000,1) 
+    def props(self):
+        data = {
+            "type": self.type,
+            "red": self.hue,
+            "blue": self.saturation,
+            "green": self.lightness
+        } 
+        return data
+
+##Colour List
+colour_array=[RGB, HSL]  
+
+def random_colour(x=random.randrange(0,len(colour_array))):
+    if x >= len(colour_array):
+        raise Exception
+    
+    return colour_array[x]()
--- backend/backend/views.py
+++ backend/backend/views.py
@@ -1,7 +1,19 @@
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from . import colours
 
+
+@csrf_exempt
 def colour_picker(request):
     if request.method == 'GET': 
+        #Generate random colours
+        print(len(colours.array))
+        new_colours = []
+        for i in range(0,5): 
+            colour = colours.random_colour()
+            new_colours.append(colour.props())
+
+        print(new_colours)
         return HttpResponse('OK', status=200)
     else: 
         return HttpResponse(status=404)
